20a8d7987d798d0d51c5a2a9a8ae21762e533fb6
Allan-Gakuru <allan.gakuru@outlook.com>
2022-03-10 18:59:02 -0800

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
 * main - prints the phrase "Programming is like building a multilingual
  *   * Return: 0 if exited properly, non-zero otherwise
-* *   */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
